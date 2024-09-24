1746c33dec5e6a6052b1b6e09361aa80c79ff1c1
cjburn3 <156855354+cjburn3@users.noreply.github.com>
2024-09-24 19:56:57 -0400

    updates

--- __tests__/snacks.test.js
+++ __tests__/snacks.test.js
@@ -6,9 +6,10 @@ describe('Snacks API', () => {
     const response = await request(app)
     .get('/snacks')
     .set('x-api-key', process.env.API_KEY);
-    
+
     expect(response.statusCode).toBe(200);
     expect(response.body).toBeDefined();
   });
 
 });
+
